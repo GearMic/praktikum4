b4f328968cb5201e8b2afcd3dd42a5db63fa84e1
GearMic <mic.lp281@gmail.com>
2024-11-05 11:45:42 +0100

    p401 rework peak locaiton

--- p401/src/peak_location.py
+++ p401/src/peak_location.py
@@ -13,9 +13,9 @@ def array_range(array, overhang=0.05, nPoints=100):
 
 def load_data(filename, yErr):
     data = pd.read_csv(filename, sep='\t', encoding_errors='replace')
-    alpha = data.iloc[:, 0]
-    y = data.iloc[:, 1]
-    yErr = np.full_like(y, yErr)
+    alpha = np.array(data.iloc[:, 0])
+    y = np.array(data.iloc[:, 1])
+    yErr = np.array(np.full_like(y, yErr))
     # yErr = np.maximum(y*errorPortion, errorMin)
 
     return alpha, y, yErr
@@ -23,38 +23,61 @@ def load_data(filename, yErr):
 def gauss_fn(x, a, b, c):
     return a * np.exp(-(x-b)**2/ (2*c**2))
 
-def triple_gauss_fn(alpha, a1, b1, c1, a2, b2, c2, a3, b3, c3):
-    return gauss_fn(alpha, a1, b1, c1) + gauss_fn(alpha, a2, b2, c2) + gauss_fn(alpha, a3, b3, c3)
+def triple_gauss_fn(alpha, a1, b1, c1, a2, b2, c2, a3, b3, c3, B):
+    return B + gauss_fn(alpha, a1, b1, c1) + gauss_fn(alpha, a2, b2, c2) + gauss_fn(alpha, a3, b3, c3)
 
-def fit_ccd_data(alpha, y, yErr):
-    popt, pcov = optimize.curve_fit(triple_gauss_fn, alpha, y, sigma=yErr)
+def fit_ccd_data(alpha, y, yErr, p0=None):
+    popt, pcov = optimize.curve_fit(triple_gauss_fn, alpha, y, sigma=yErr, p0=p0, maxfev=10000)
+    # popt, pcov = optimize.curve_fit(gauss_fn, alpha, y, sigma=yErr, maxfev=10000)
     params, paramsErr = popt, np.sqrt(np.diag(pcov))
 
     return params, paramsErr
 
-def plot_data_fit(fig, ax, alpha, y, yErr):
+def plot_data_fit(fig, ax, alpha, y, yErr, params):
     # ax.errorbar(alpha, y, yErr, label='Daten')
     ax.plot(alpha, y, '-', lw=1, label='Daten')
 
-    # params, paramsErr = fit_ccd_data(alpha, y, yErr)
-    # # paramsPrint = np.array((params, paramsErr)).T
-    # # paramsPrint = (r' \pm '.join(tuple(np.array(param, dtype=str))) for param in paramsPrint)
-    # # paramsPrint = r',\ '.join(paramsPrint)
-    # # print(paramsPrint)
-    # print(params)
-    # print(paramsErr)
+    # paramsPrint = np.array((params, paramsErr)).T
+    # paramsPrint = (r' \pm '.join(tuple(np.array(param, dtype=str))) for param in paramsPrint)
+    # paramsPrint = r',\ '.join(paramsPrint)
+    # print(paramsPrint)
 
-    # xFit = array_range(alpha, overhang=0)
-    # yFit = field_fit_fn(xFit, *params)
-    # ax.plot(xFit, yFit, label='Kalibrierungskurve')
+    xFit = array_range(alpha, overhang=0)
+    yFit = triple_gauss_fn(xFit, *params)
+    ax.plot(xFit, yFit, label='Kalibrierungskurve')
 
 # alpha, y, yErr, load_data('p401/data/interference_9.1A.txt', 0.02, 0.5)
-alpha, y, yErr = load_data('p401/data/interference_9.1A.txt', 1)
+# alpha, y, yErr = load_data('p401/data/interference_9.1A.txt', 1)
 
-fig, ax = plt.subplots()
-plot_data_fit(fig, ax, alpha, y, yErr)
-ax.minorticks_on()
-ax.grid(which='both')
-fig.savefig('p401/plot/gauss_1.pdf')
+# fig, ax = plt.subplots()
+# plot_data_fit(fig, ax, alpha, y, yErr)
+# ax.minorticks_on()
+# ax.grid(which='both')
+# fig.savefig('p401/plot/gauss_1.pdf')
 
-inFilenames = tuple('p401/data/interference_%1fA.txt' % Ival for Ival in I)
+I = np.array((2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 7.5, 8.0, 8.5, 9.1))
+alphaRange = np.array(((0.77, 1.16), (0.77, 1.16), (0.77, 1.16), (0.77, 1.16), (0.77, 1.16), (0.77, 1.16), (0.77, 1.16), (0.77, 1.16), (0.77, 1.16), (0.77, 1.16)))
+inFilenames = tuple('p401/data/interference_%.1fA.txt' % Ival for Ival in I)
+outFilenames = tuple('p401/plot/gauss_%.1fA.pdf' % Ival for Ival in I)
+
+params, paramsErr = np.zeros((len(I), 10)), np.zeros((len(I), 10))
+for i in range(len(inFilenames)):
+# for i in range(9, 10):
+    lower, upper = alphaRange[i, 0], alphaRange[i, 1]
+
+    alpha, y, yErr = load_data(inFilenames[i], 1)
+    rangeMask = (alpha >= np.full_like(alpha, lower)) & (alpha <= np.full_like(alpha, upper))
+    alpha, y, yErr = alpha[rangeMask], y[rangeMask], yErr[rangeMask]
+
+    # fit data
+    param, paramErr = fit_ccd_data(alpha, y, yErr, p0=(40, 0.85, 0.05, 50, 0.97, 0.05, 40, 1.09, 0.05, 1))
+    params[i] = param
+    paramsErr[i] = paramErr
+
+    fig, ax = plt.subplots()
+
+    plot_data_fit(fig, ax, alpha, y, yErr, param)
+    ax.legend()
+    ax.minorticks_on()
+    ax.grid(which='both')
+    fig.savefig(outFilenames[i])
