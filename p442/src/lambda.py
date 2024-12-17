331abe92fb225b040038fb18cbc0badff6dbae5c
GearMic <mic.lp281@gmail.com>
2024-12-17 14:40:41 +0100

    lambda fit done

--- p442/src/lambda.py
+++ p442/src/lambda.py
@@ -20,21 +20,22 @@ alpha = 0
 def pos_to_cos(n, x, xErr):
     # x = np.maximum(x, 0.0001)
     beta = np.arctan(x/d)
-    print(beta)
 
-    betaErr = np.minimum(1/d/(1*(x/d)**2), 0.1)
-    cosBeta = np.cos(beta)
+    # betaErr = np.minimum(1/d/(1*(x/d)**2), 0.1)
+    # betaErr = 1/d/(1+(x/d)**2)
+    betaErr = (1+(x/d)**2) * np.sqrt( (xErr/d)**2 + (dErr*x/d**2)**2 )
+    sinBeta = np.sin(beta)
 
-    return cosBeta, np.abs(np.sin(beta)*betaErr)
+    return sinBeta, np.abs(np.cos(beta)*betaErr)
 
 
 n, x, xErr = load_data('p442/data/5.3lambda.csv')
-print(x)
-nAbs = np.abs(n)
+# nAbs = np.abs(n)
+nAbs = n
 cosx, cosxErr = pos_to_cos(n, x, xErr)
-print(cosx)
-print('test', np.arctan(-1/3), np.arctan(1/3))
-
+print('n', n)
+print('sin', cosx)
+print('Delta sin', cosxErr)
 
 #fit 1
 params, paramsErr = odr_fit(linear_fn_odr, cosx, nAbs, 2, xErr=cosxErr, p0=(1, -1))
@@ -44,16 +45,17 @@ yFit = linear_fn_odr(params, xFit)
 
 plt.errorbar(cosx, nAbs, 0, cosxErr, fmt='x')
 plt.plot(xFit, yFit)
-plt.xlabel('cos(beta)')
+plt.xlabel('sin(beta)')
 plt.ylabel('|n|')
 plt.savefig('p442/plot/5.3lambda.pdf')
 
-m = params[1]
-lbda = -g/m
-print(params, paramsErr)
-print('m, lbda', m, lbda)
+m, mErr = params[1], paramsErr[1]
+lbda = g/m
+lbdaErr = mErr * g/m**2
+print('params', params, paramsErr)
+print('lbda', lbda, lbdaErr)
 
-# fit 2
+""" # fit 2
 params, paramsErr = odr_fit(linear_fn_odr, nAbs, cosx, 2, yErr=cosxErr, p0=(1, -1))
 
 xFit = array_range(nAbs)
@@ -63,10 +65,10 @@ plt.clf()
 plt.errorbar(nAbs, cosx, cosxErr, fmt='x')
 plt.plot(xFit, yFit)
 plt.xlabel('|n|')
-plt.ylabel('cos(beta)')
+plt.ylabel('sin(beta)')
 plt.savefig('p442/plot/5.3lambda2.pdf')
 
 m = params[1]
 lbda = -m*g
 print(params, paramsErr)
-print('m, lbda', m, lbda)
+print('m, lbda', m, lbda) """
