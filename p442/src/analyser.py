9914b895b2333316afe5c206fe3c241b762ca5ff
GearMic <mic.lp281@gmail.com>
2024-12-17 08:58:48 +0100

    analyser done mostly

--- p442/src/analyser.py
+++ p442/src/analyser.py
@@ -21,6 +21,11 @@ def preprocess_data(leftIndex, rightIndex):
     ratioErr = np.sqrt(2*(xErr/fsr)**2 + ((x-left)*fsrErr/fsr**2)**2)
     deltaNu = ratio*modeSep
     deltaNuErr = ratioErr*modeSep
+    laserFsr = c/2/L
+    laserFsrErr = c/2/L**2*Lerr
+    print('Laser FSR:', laserFsr/1e6, laserFsrErr/1e6)
+    laserFsrRatio = deltaNu / laserFsr
+    laserFsrRatioErr = np.sqrt( (deltaNu / laserFsr**2 * laserFsrErr)**2 + (deltaNuErr/laserFsr)**2 )
 
     print('fsr:', fsr, fsrErr)
     print('ratio', ratio)
@@ -31,12 +36,14 @@ def preprocess_data(leftIndex, rightIndex):
     dataFrame = pd.DataFrame({
         r'Nr.': nr,
         r'$x/\mathrm{Skt.}$': x,
-        r'$\delta\nu/\mathrm{FSR}$': ratio,
-        r'$\Delta\delta\nu/\mathrm{FSR}$': ratioErr,
+        r'$\delta\nu/\mathrm{MD}$': ratio,
+        r'$\Delta\delta\nu/\mathrm{MD}$': ratioErr,
         # r'$\delta\nu/\si\MHz$': deltaNu/1e6,
         # r'$\Delta\delta\nu/\si\MHz$': deltaNuErr/1e6
         r'$\delta\nu$': deltaNu/1e6,
-        r'$\Delta\delta\nu$': deltaNuErr/1e6
+        r'$\Delta\delta\nu$': deltaNuErr/1e6,
+        r'$\delta\nu/\mr{FSR}_\mr{Laser}$': laserFsrRatio,
+        r'$\Delta\delta\nu/\mr{FSR}_\mr{Laser}$': laserFsrRatioErr,
     })
 
     # return ratio, deltaNu, fsr, ratioErr, deltaNuErr, fsrErr
@@ -48,12 +55,14 @@ modeSep = c/l/4
 xErr = 0.3
 
 ## short resonator
+L, Lerr = 48.6e-2, 0.1e-2
 nr, x = load_data('p442/data/5.7analyser_short.csv')
 dataFrame = preprocess_data(1, 4)
 dataFrame.to_latex('p442/data/5.7analyser_short.auto.tex', index=False)
 # TODO: calculate ratio to laser FSR
 
 ## long resonator
+L, Lerr = 61.5e-2, 0.1e-2
 nr, x = load_data('p442/data/5.7analyser_long.csv')
 dataFrame = preprocess_data(0, -1)
 dataFrame.to_latex('p442/data/5.7analyser_long.auto.tex', index=False)
